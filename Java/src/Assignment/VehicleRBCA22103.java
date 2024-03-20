46f41ff7ba2c27cb973abb3e57adb81acf805465
Abdul hadi <abdulhadiqureashi10@gmail.com>
2024-03-20 11:05:24 +0530

    Commit

---
+++ Java/src/Assignment/VehicleRBCA22103.java
@@ -0,0 +1,21 @@
+package Assignment;
+
+public class VehicleRBCA22103 {
+
+	    private int horsepower;
+
+	    public VehicleRBCA22103 (int horsepower) {
+	        this.horsepower = horsepower;
+	    }
+
+	    public void drive() {
+	        System.out.println("Vehicle is being driven");
+	    }
+
+	    public int getHorsepower() {
+	        return horsepower;
+	    }
+	}
+
+
+
