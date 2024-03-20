01c244f110678e076c7a6b168a3e9c1edbfcff1d
Abdul hadi <abdulhadiqureashi10@gmail.com>
2024-03-20 11:23:26 +0530

    Commit

---
+++ Java/src/Assignment/MainShapeRBCA22103.java
@@ -0,0 +1,25 @@
+package Assignment;
+
+import java.text.DecimalFormat;
+
+public class MainShapeRBCA22103 {
+	    public static void main(String[] args) {
+	        Circle circle = new Circle(5);
+System.out.println("Circle - Area: " + formatDecimal(circle.calculateArea()) + ", Perimeter: " + formatDecimal(circle.calculatePerimeter()));
+
+	
+	        Triangle triangle = new Triangle(3, 4, 5);
+	        System.out.println("Triangle - Area: " + formatDecimal(triangle.calculateArea()) + ", Perimeter: " + formatDecimal(triangle.calculatePerimeter()));
+	    }
+
+	    private static String formatDecimal(double value) {
+	        DecimalFormat df = new DecimalFormat("#.##");
+	        return df.format(value);
+	    }
+	}
+
+
+	
+
+
+
