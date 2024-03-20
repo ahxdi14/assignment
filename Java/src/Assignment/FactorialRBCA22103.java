f5c49ecec69429a9c23a481ff15b1360ba7d216f
Abdul hadi <abdulhadiqureashi10@gmail.com>
2024-03-20 09:52:07 +0530

    Commit

---
+++ Java/src/Assignment/FactorialRBCA22103.java
@@ -0,0 +1,16 @@
+package Assignment;
+
+public class FactorialRBCA22103 {
+	    public int calculateFactorial(int n) {
+	        if (n < 0)
+	            throw new IllegalArgumentException("Factorial is not defined for negative numbers");
+	        int factorial = 1;
+	        for (int i = 1; i <= n; i++) {
+	            factorial *= i;
+	        }
+	        return factorial;
+	    }
+	}
+
+
+
