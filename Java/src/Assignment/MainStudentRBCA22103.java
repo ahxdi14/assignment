2ecb5636140f60d2c1079d1fecf08ae11e86d747
Abdul hadi <abdulhadiqureashi10@gmail.com>
2024-03-20 11:14:41 +0530

    Commit

---
+++ Java/src/Assignment/MainStudentRBCA22103.java
@@ -0,0 +1,32 @@
+package Assignment;
+
+public class MainStudentRBCA22103 {
+	    public static void main(String[] args) {
+	        // Creating instances of Student class
+	    	StudentRBCA22103  student1 = new StudentRBCA22103 ();
+	    	StudentRBCA22103  student2 = new StudentRBCA22103 ();
+	    	StudentRBCA22103 student3 = new StudentRBCA22103 ();
+
+	        // Setting information for student1
+	        student1.setInfo("John", 20);
+
+	        // Setting information for student2
+	        student2.setInfo("Alice", 22, "123 Main St");
+
+	        // Setting information for student3
+	        student3.setInfo("Bob", 21, "456 Oak Ave");
+
+	        // Printing information of all students
+	        System.out.println("Information of Student 1:");
+	        student1.printInfo();
+
+	        System.out.println("Information of Student 2:");
+	        student2.printInfo();
+
+	        System.out.println("Information of Student 3:");
+	        student3.printInfo();
+	    }
+	}
+
+
+
